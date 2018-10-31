b6134fe3ee27d4f782e394b6fed5b44ee5c98425
taruc <taruc@PC183>
2018-10-31 15:18:31 +0800

    Hello Jing Min

---
+++ src/gsample1/GSample1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gsample1;
+
+/**
+ *
+ * @author taruc
+ */
+public class GSample1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+        System.out.println("My name is Loo Mae Yin.");
+        System.out.println("I currenly studying Software System Development in TAR UC");
+        // TODO code application logic here
+    }
+    
+}
+    
+ 
